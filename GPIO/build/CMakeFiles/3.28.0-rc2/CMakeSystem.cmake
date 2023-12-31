0c2bda89edb057f395faf4a1d6c3ce921dfbc9f6
Pansamic <pansamic@foxmail.com>
2023-12-31 08:43:02 +0800

    unify code and files

---
+++ GPIO/build/CMakeFiles/3.28.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("D:/Document/EE/MCU/MSPM0/MSPM0-DevKit/GPIO/toolchains/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ GPIO/build/CMakeFiles/mspm0_gpio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mspm0_gpio.
+# This may be replaced when dependencies are built.
